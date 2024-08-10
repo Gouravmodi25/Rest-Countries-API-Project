94697ecefea7027a0885d0acc47ecb60061f4496
Gouravmodi25 <gouravmodi39@gmail.com>
2024-08-11 00:21:14 +0530

    give logic to our search by countries name

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 const countryContainer = document.querySelector(".countries-container");
+const filterByRegion = document.querySelector(".filter-by-region");
+const searchInput = document.querySelector(".search-container input");
+let allCountriesData;
 fetch("https://restcountries.com/v3.1/all")
   .then((response) => response.json())
   .then((data) => {
-    data.forEach((country) => {
-      if (country.borders) {
-        console.log(country.borders);
-      }
+    renderCountries(data);
+    allCountriesData = data;
+  });
 
-      const countryCard = document.createElement("a");
-      countryCard.classList.add("country-card");
-      countryCard.href = `country.html?name=${country.name.common}`;
-      const cardHtml = ` 
+function renderCountries(data) {
+  countryContainer.innerHTML = "";
+  data.forEach((country) => {
+    const countryCard = document.createElement("a");
+    countryCard.classList.add("country-card");
+    countryCard.href = `country.html?name=${country.name.common}`;
+    const cardHtml = ` 
         <img id="flag" src="${country.flags.svg}" alt="${
-        country.name.common
-      }-flag" />
+      country.name.common
+    }-flag" />
         <div class="card-text">
           <h3 id="card-title" class="card-title">${country.name.common}</h3>
           <p id="population"><b>Population:</b> ${country.population.toLocaleString(
@@ -22,7 +27,25 @@ fetch("https://restcountries.com/v3.1/all")
           <p id="region"><b>Region:</b> ${country.region}</p>
           <p id="capital"><b>Capital:</b> ${country.capital?.[0]}</p>
         </div>`;
-      countryCard.innerHTML = cardHtml;
-      countryContainer.appendChild(countryCard);
-    });
+    countryCard.innerHTML = cardHtml;
+    countryContainer.appendChild(countryCard);
   });
+}
+
+filterByRegion.addEventListener("change", (e) => {
+  console.log(filterByRegion.value);
+  fetch(`https://restcountries.com/v3.1/region/${filterByRegion.value}`)
+    .then((response) => response.json())
+    .then((data) => {
+      renderCountries(data);
+    });
+});
+
+searchInput.addEventListener("input", (e) => {
+  const filteredCountries = allCountriesData.filter((country) => {
+    return country.name.common
+      .toLowerCase()
+      .includes(e.target.value.toLowerCase());
+  });
+  renderCountries(filteredCountries);
+});
